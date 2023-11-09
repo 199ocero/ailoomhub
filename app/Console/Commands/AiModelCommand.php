e041dacfe0a2848e96f5a7a099dfd4900d7ef4b5
199ocero <199ocero@gmail.com>
2023-11-09 21:47:50 +0800

    added commands for provider and model

---
+++ app/Console/Commands/AiModelCommand.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\AiModel;
+use App\Models\Provider;
+use Illuminate\Console\Command;
+use function Laravel\Prompts\info;
+use function Laravel\Prompts\text;
+use function Laravel\Prompts\select;
+
+class AiModelCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'make:ai-model';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Make an AI model';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $provider = select(
+            label: 'Select a Provider',
+            options: Provider::query()->pluck('name', 'id'),
+            required: 'Provider is required.',
+            scroll: 10
+        );
+
+        $name = text(
+            label: 'What is the name of the AI model?',
+            placeholder: 'E.g. gpt-3.5-turbo',
+            required: 'Name is required.'
+        );
+
+        $aiName = AiModel::query()->where('name', $name)->first();
+
+        if ($aiName) {
+            info($name . ' model already exists!');
+            return;
+        }
+
+        $model = AiModel::query()
+            ->where('provider_id', $provider)
+            ->where('name', $name)
+            ->first();
+
+        if ($model) {
+            info($name . ' provider and model already exists!');
+            return;
+        }
+
+        AiModel::query()->create([
+            'provider_id' => $provider,
+            'name' => $name,
+        ]);
+
+        info($name . ' model created successfully!');
+    }
+}
---
+++ app/Console/Commands/AiProviderCommand.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Provider;
+use Illuminate\Console\Command;
+use function Laravel\Prompts\info;
+use function Laravel\Prompts\text;
+
+class AiProviderCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'make:ai-provider';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Make an AI provider';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $name = text(
+            label: 'What is the name of the AI provider?',
+            placeholder: 'E.g. OpenAI',
+            required: 'Name is required.'
+        );
+
+        $provider = Provider::where('name', $name)->first();
+
+        if ($provider) {
+            info($name . ' provider already exists!');
+            return;
+        }
+
+        Provider::query()->create([
+            'name' => $name,
+        ]);
+
+        info($name . ' provider created successfully!');
+    }
+}
