ded1c1b06b48e6045387a8ab8007c151a61541ef
199ocero <199ocero@gmail.com>
2023-11-09 19:07:37 +0800

    added loom agent

---
+++ app/Models/LoomAgent.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class LoomAgent extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'name',
+        'provider',
+        'model',
+        'system_message',
+        'knowledge_base',
+        'status',
+        'config_options',
+        'usage_count',
+        'last_used',
+        'token',
+    ];
+
+    /**
+     * Get the user that owns the LoomAgent
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
---
+++ database/migrations/2023_11_09_105015_create_loom_agents_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('loom_agents', function (Blueprint $table) {
+            $table->id();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->string('name');
+            $table->string('provider');
+            $table->string('model');
+            $table->string('system_message');
+            $table->string('knowledge_base');
+            $table->boolean('status')->default(true);
+            $table->json('config_options')->nullable();
+            $table->integer('usage_count')->default(0);
+            $table->timestamp('last_used')->nullable();
+            $table->text('token');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('loom_agents');
+    }
+};
