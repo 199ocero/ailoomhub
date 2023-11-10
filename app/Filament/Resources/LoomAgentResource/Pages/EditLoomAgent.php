99269b3031b0ff25c01ea47b9382702579913ff4
199ocero <199ocero@gmail.com>
2023-11-10 20:00:18 +0800

    remove delete action and add redirect url

--- app/Filament/Resources/LoomAgentResource/Pages/EditLoomAgent.php
+++ app/Filament/Resources/LoomAgentResource/Pages/EditLoomAgent.php
@@ -10,10 +10,8 @@ class EditLoomAgent extends EditRecord
 {
     protected static string $resource = LoomAgentResource::class;
 
-    protected function getHeaderActions(): array
+    protected function getRedirectUrl(): string
     {
-        return [
-            Actions\DeleteAction::make(),
-        ];
+        return $this->getResource()::getUrl('index');
     }
 }
