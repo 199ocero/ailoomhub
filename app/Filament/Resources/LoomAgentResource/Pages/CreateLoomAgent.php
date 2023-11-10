9b0534b9850f45bbac51bec0a529d49c681d5dba
199ocero <199ocero@gmail.com>
2023-11-10 17:41:20 +0800

    added user id before saving

--- app/Filament/Resources/LoomAgentResource/Pages/CreateLoomAgent.php
+++ app/Filament/Resources/LoomAgentResource/Pages/CreateLoomAgent.php
@@ -9,4 +9,11 @@ use Filament\Resources\Pages\CreateRecord;
 class CreateLoomAgent extends CreateRecord
 {
     protected static string $resource = LoomAgentResource::class;
+
+    protected function mutateFormDataBeforeCreate(array $data): array
+    {
+        $data['user_id'] = auth()->id();
+
+        return $data;
+    }
 }
