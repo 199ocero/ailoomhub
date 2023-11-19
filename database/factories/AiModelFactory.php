72335dfb4714314f43b17799766408363bb0b1b0
199ocero <199ocero@gmail.com>
2023-11-19 08:52:23 +0800

    added factory and seeders

---
+++ database/factories/AiModelFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\AiModel>
+ */
+class AiModelFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'ai_provider_id' => 1,
+            'name' => 'gpt-3.5-turbo',
+        ];
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,9 +15,23 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        \App\Models\User::factory()->create([
+            'name' => 'Admin',
+            'email' => 'admin@gmail.com',
+            'password' => Hash::make('password'),
+        ]);
+
+        $provider = \App\Models\AiProvider::factory()->create([
+            'name' => 'OpenAI',
+        ]);
+
+        \App\Models\AiModel::factory()->create([
+            'ai_provider_id' => $provider->id,
+            'name' => 'gpt-3.5-turbo',
+        ]);
+
+        \App\Models\KnowledgeBase::factory()->create([
+            'name' => 'Notion',
+        ]);
     }
 }
